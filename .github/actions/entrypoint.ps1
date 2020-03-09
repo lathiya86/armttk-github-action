165f4f3eaa3ab049cecbd079de57b4709fe9896f
whaakman <whaakman@intercept.nl>
2020-03-09 20:58:32 +0100

    Initial commit

---
+++ .github/actions/entrypoint.ps1
@@ -0,0 +1,12 @@
+# Import the arm-ttk module we cloned in the dockerfile
+Import-Module '/arm-ttk/arm-ttk/arm-ttk.psd1'
+
+# Path we cloned the repository into
+$TemplatePath = '/temp/'
+
+$TestResults = Test-AzTemplate -TemplatePath $TemplatePath
+# We only want to return failures
+$TestFailures =  $TestResults | Where-Object { -not $_.Passed }
+Write-host "::set-output name=results::Hello!"
+Write-Output $TestFailures
+exit 1
